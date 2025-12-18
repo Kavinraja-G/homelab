3526562185c229295c8122aba64780861dd627e6
Kavin <62742678+Kavinraja-G@users.noreply.github.com>
2025-12-18 23:54:30 +0530

    Move to 3 CP and 1 node cluster

--- infrastructure/talos.tf
+++ infrastructure/talos.tf
@@ -17,31 +17,51 @@ module "talos" {
 
   cluster = {
     name            = "homelab-k8s"
-    endpoint        = "192.168.0.70"
-    gateway         = "192.168.0.1"
+    endpoint        = "192.168.20.115"
+    gateway         = "192.168.20.1"
     talos_version   = "v1.11.5"
     proxmox_cluster = "homelab"
   }
 
   nodes = {
-    "controlplane-01" = {
-      host_node     = "thinkcentre-01"
+    "cp-node-01" = {
+      host_node     = "cp-node-01"
       machine_type  = "controlplane"
-      ip            = "192.168.0.70"
+      ip            = "192.168.20.115"
       mac_address   = "BC:24:11:2E:C7:01"
       vm_id         = 701
       datastore_id  = "local-lvm"
       cpu           = 4
       ram_dedicated = 8192
     }
-    "worker-01" = {
-      host_node     = "sff-linux-01"
-      machine_type  = "worker"
-      ip            = "192.168.0.80"
+    "cp-node-02" = {
+      host_node     = "cp-node-02"
+      machine_type  = "controlplane"
+      ip            = "192.168.20.125"
       mac_address   = "BC:24:11:2E:C7:02"
-      vm_id         = 801
+      vm_id         = 702
       datastore_id  = "local-lvm"
       cpu           = 4
+      ram_dedicated = 8192
+    }
+    "cp-node-03" = {
+      host_node     = "node-01"
+      machine_type  = "controlplane"
+      ip            = "192.168.20.135"
+      mac_address   = "BC:24:11:2E:C7:03"
+      vm_id         = 703
+      datastore_id  = "local-lvm"
+      cpu           = 2
+      ram_dedicated = 4096
+    }
+    "worker-01" = {
+      host_node     = "node-01"
+      machine_type  = "worker"
+      ip            = "192.168.20.140"
+      mac_address   = "BC:24:11:2E:C8:01"
+      vm_id         = 801
+      datastore_id  = "local-lvm"
+      cpu           = 2
       ram_dedicated = 8192
     }
   }
