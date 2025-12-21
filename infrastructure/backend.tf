c1fc3e9eadf9b43a7ff9483b2b73872bd5c6fad1
Kavin <62742678+Kavinraja-G@users.noreply.github.com>
2025-12-21 12:27:06 +0530

    Fix remote backend using sops

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "http" {
-    address = "http://localhost:6061/?type=git&repository=git@github.com:Kavinraja-G/homelab-tofu-state&ref=main&state=my/homelab/main-infra-state.json"
-    lock_address = "http://localhost:6061/?type=git&repository=git@github.com:Kavinraja-G/homelab-tofu-state&ref=main&state=my/homelab/main-infra-state.json"
-    unlock_address = "http://localhost:6061/?type=git&repository=git@github.com:Kavinraja-G/homelab-tofu-state&ref=main&state=my/homelab/main-infra-state.json"
+    address = "http://localhost:6061/?type=git&repository=git@github.com:Kavinraja-G/homelab-tofu-state&ref=main&state=homelab/main-infra-state.json"
+    lock_address = "http://localhost:6061/?type=git&repository=git@github.com:Kavinraja-G/homelab-tofu-state&ref=main&state=homelab/main-infra-state.json"
+    unlock_address = "http://localhost:6061/?type=git&repository=git@github.com:Kavinraja-G/homelab-tofu-state&ref=main&state=homelab/main-infra-state.json"
   }
 }
